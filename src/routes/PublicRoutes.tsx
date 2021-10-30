9439ba3ac4573093a59d5a341572ace670a992cf
Arthur Brito Medeiros <arthurmedeiros32@gmail.com>
2021-10-30 01:05:09 -0300

    Feature(login-screen): make home screen with google loginand library for it

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import { HomePage, LoginAuth } from '../components';
+import { HomePage } from '../components';
 
 export const PublicRoutes = (): JSX.Element => (
   <Switch>
     <Route path="/" exact>
       <HomePage />
-      <LoginAuth />
     </Route>
   </Switch>
 );
