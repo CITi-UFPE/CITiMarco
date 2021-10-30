0abf83f7a23b699068a2705d3d99c129ca59a2e9
Arthur Brito Medeiros <47860925+ArthurBM@users.noreply.github.com>
2021-10-30 00:53:06 -0300

    Feature/google login (#25)
    * Feature(google-login): add login and logout components of Oauth
    
    * Hotfix(google-login): add .env to project
    
    Co-authored-by: Shellyda <shellydabarbosa@gmail.com>

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,11 @@
 import { HomePage } from './HomePage';
+import LoginAuth from './LoginAuth';
+import LogoutAuth from './LogoutAuth';
 import { DateRangePicker } from './DateRangePicker';
 
 export {
   HomePage,
+  LoginAuth,
+  LogoutAuth,
   DateRangePicker,
 };
